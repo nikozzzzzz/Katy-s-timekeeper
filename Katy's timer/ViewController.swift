22e942b303ec0002ca6a8303a3c368e88839c27b
Nikos Papadopulos <nikozzzzzz@gmail.com>
2018-08-19 06:18:48 -0700

    added functionality, fixed typos

--- Katy's timer/ViewController.swift
+++ Katy's timer/ViewController.swift
@@ -10,25 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     func runTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: false)
+        timer1 = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
     }
-    func updateTimer() {
+    func timeString(time:TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format:"%02i:%02i", minutes, seconds)
+    }
+    @objc func updateTimer() {
         seconds -= 1     //This will decrement(count down)the seconds.
-        segwayX2TimeLabel.text = “\(seconds)” //This will update the label.
+        segwayX2TimeLabel.text = timeString(time:TimeInterval (seconds)) //This will update the label.
+        print(timeString(time:TimeInterval (seconds)))
     }
     
+    //First rent object
     @IBOutlet weak var segwayX2TimeLabel: UILabel!
     var seconds = 600 // time to go
-    var timer = Timer()
+    var timer1 = Timer()
     var isTimerRunning = false //check if timer is already running
     
     @IBAction func segwayX2PlayButton(_ sender: UIButton) {
+        runTimer()
+        
     }
     
     @IBAction func segwayX2CancelButton(_ sender: UIButton) {
+        timer1.invalidate()
         seconds = 600    // come to this point in time when cancel button is pressed
-        segwayX2TimeLabel.text = “\(seconds)”
+        segwayX2TimeLabel.text = timeString(time: TimeInterval(seconds))
     }
+    
+    
+    //Second rent object
     
     
     
