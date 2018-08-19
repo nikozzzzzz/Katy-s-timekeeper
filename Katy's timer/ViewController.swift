4fe4a0e450e1f2cbc6c86dc76ec39a9bf1f5de9d
Nikos Papadopulos <nikozzzzzz@gmail.com>
2018-08-19 12:40:48 -0700

    fixed timer running multiple times in 1 instance

--- Katy's timer/ViewController.swift
+++ Katy's timer/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     //functions
     func runTimer() {
         timer1 = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+        isTimerRunning = true
     }
     
     func timeString(time:TimeInterval) -> String {
@@ -34,7 +35,9 @@ class ViewController: UIViewController {
     var isTimerRunning = false //check if timer is already running
     var seconds = 600 // time to go
     @IBAction func segwayX2PlayButton(_ sender: UIButton) {
+        if isTimerRunning == false {
         runTimer()
+        }
         
     }
     
@@ -42,6 +45,7 @@ class ViewController: UIViewController {
         timer1.invalidate()
         seconds = 600    // come to this point in time when cancel button is pressed
         segwayX2TimeLabel.text = timeString(time: TimeInterval(seconds))
+        isTimerRunning = false
     }
     
     
@@ -49,6 +53,7 @@ class ViewController: UIViewController {
     
     func runTimer2() {
         timer2 = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer2)), userInfo: nil, repeats: true)
+        isTimer2Running = true
         
     }
     @objc func updateTimer2() {
@@ -61,7 +66,9 @@ class ViewController: UIViewController {
     var timer2 = Timer()
     var isTimer2Running = false
     @IBAction func secondPlayButton(_ sender: UIButton) {
+        if isTimer2Running == false {
         runTimer2()
+        }
         
     }
     
@@ -69,11 +76,13 @@ class ViewController: UIViewController {
         timer2.invalidate()
         seconds2 = 600
         secondTimeLabel.text = timeString(time: TimeInterval(seconds2))
+        isTimer2Running = false
     }
     
     //Third timer
     func runTimer3() {
         timer3 = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer3)), userInfo: nil, repeats: true)
+        isTimer3Running = true
         
     }
     @objc func updateTimer3() {
@@ -88,13 +97,16 @@ class ViewController: UIViewController {
     var isTimer3Running = false
     
     @IBAction func thirdPlayButton(_ sender: UIButton) {
+        if isTimer3Running == false {
         runTimer3()
+        }
     }
     
     @IBAction func thirdCancelButton(_ sender: UIButton) {
         timer3.invalidate()
         seconds3 = 600
         thirdTimeLabel.text = timeString(time: TimeInterval(seconds3))
+        isTimer3Running = false
     }
     //Fourth timer
     @IBOutlet weak var fourthTimeLabel: UILabel!
@@ -104,6 +116,7 @@ class ViewController: UIViewController {
     func runTimer4() {
         
         timer4 = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer4)), userInfo: nil, repeats: true)
+        isTimer4Running = true
     }
     @objc func updateTimer4() {
         seconds4 -= 1
@@ -112,13 +125,16 @@ class ViewController: UIViewController {
     }
     
     @IBAction func fourthPlayButton(_ sender: UIButton) {
+        if isTimer4Running == false {
         runTimer4()
+        }
     }
     
     @IBAction func fourthCancelButton(_ sender: UIButton) {
         timer4.invalidate()
         seconds4 = 600
         fourthTimeLabel.text = timeString(time: TimeInterval(seconds4))
+        isTimer4Running = false
     }
     
     //Fifth timer
@@ -128,6 +144,7 @@ class ViewController: UIViewController {
     var isTimer5Running = false
     func runTimer5() {
         timer5 = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer5)), userInfo: nil, repeats: true)
+        isTimer5Running = true
         
     }
     @objc func updateTimer5() {
@@ -136,13 +153,16 @@ class ViewController: UIViewController {
        // print(timeString(time:TimeInterval (seconds)))
     }
     @IBAction func fifthPlayButton(_ sender: UIButton) {
+        if isTimer5Running == false {
         runTimer5()
+        }
         
     }
     @IBAction func fifthCancelButton(_ sender: UIButton) {
         timer5.invalidate()
         seconds5 = 600
         fifthTimeLabel.text = timeString(time: TimeInterval(seconds5))
+         isTimer5Running = false
 
     }
     
@@ -154,7 +174,7 @@ class ViewController: UIViewController {
     var isTimer6Running = false
     func runTimer6() {
         timer6 = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer6)), userInfo: nil, repeats: true)
-        
+        isTimer6Running = true
     }
     @objc func updateTimer6() {
         seconds6 -= 1     //This will decrement(count down)the seconds.
@@ -162,12 +182,15 @@ class ViewController: UIViewController {
         // print(timeString(time:TimeInterval (seconds6)))
     }
     @IBAction func sixthPlayButton(_ sender: UIButton) {
+        if isTimer6Running == false {
         runTimer6()
+        }
     }
     @IBAction func sixthCancelButton(_ sender: UIButton) {
         timer6.invalidate()
         seconds6 = 600
         sixthTimeLabel.text = timeString(time: TimeInterval(seconds6))
+        isTimer6Running = false
     }
     
     
